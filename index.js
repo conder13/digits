84aa5495ebb16aebfe37438e2f55d778fc331291
conder <brenny.oshaughnessy@gmail.com>
2023-08-05 15:46:04 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 let targetNum = 105;
 let num1 = 2;
 let num2 = 3;
@@ -5,16 +6,64 @@ let num3 = 7;
 let num4 = 9;
 let num5 = 10;
 let num6 = 11;
-var moves = { 0: { but1: num1, but2: num2, but3: num3, but4: num4, but5: num5, but6: num6 } };
+
+let num1Status = true;
+let num2Status = true;
+let num3Status = true;
+let num4Status = true;
+let num5Status = true;
+let num6Status = true;
 
 var dateObj = new Date();
 var month = dateObj.getUTCMonth() + 1; //months from 1-12
-var day = dateObj.getUTCDate() - 1;
+var day = dateObj.getUTCDate();
 var year = dateObj.getUTCFullYear();
 
 newdate = year + "/" + month + "/" + day;
-function main() {
 
+let listNums = [];
+let states = [];
+let currentState = {};
+let activatedStates = [];
+let currentActivated = {};
+
+function main() {
+   console.log(newdate);
+
+   if (newdate == '2023/8/5') {
+      listNums = [213, 2, 7, 10, 16, 12, 8]
+   }
+   if (newdate == '2023/8/6') {
+      listNums = [197, 11, 4, 7, 3, 20, 17]
+   }
+   if (newdate == '2023/8/7') {
+      listNums = [255, 14, 5, 6, 8, 9, 13]
+   }
+   if (newdate == '2023/8/8') {
+      listNums = [82, 8, 13, 6, 15, 1, 2]
+   }
+   if (newdate == '2023/8/9') {
+      listNums = [454, 7, 22, 12, 11, 17, 6]
+   }
+   if (newdate == '2023/8/10') {
+      listNums = [149, 6, 15, 12, 2, 17, 4]
+   }
+   if (newdate == '2023/8/11') {
+      listNums = [84, 11, 22, 6, 2, 7, 9]
+   }
+   if (newdate == '2023/8/12') {
+      listNums = [337, 14, 7, 1, 9, 21, 8]
+   }
+   if (newdate == '2023/8/13') {
+      listNums = [237, 8, 10, 21, 6, 15, 7]
+   }
+   targetNum = listNums[0];
+   num1 = listNums[1];
+   num2 = listNums[2];
+   num3 = listNums[3];
+   num4 = listNums[4];
+   num5 = listNums[5];
+   num6 = listNums[6];
 
    document.getElementById("targetNumber").innerHTML = targetNum;
    document.getElementById("button1").innerHTML = num1;
@@ -24,45 +73,17 @@ function main() {
    document.getElementById("button5").innerHTML = num5;
    document.getElementById("button6").innerHTML = num6;
 
-   // var moves = { 0: { but1: num1, but2: num2, but3: num3, but4: num4, but5: num5, but6: num6 } };
-   //console.log(moves);
-
-   const express = require('express');
-   const { createPool } = require('mysql2');
-   const app = express();
-
-   const pool = createPool({
-      host: "localhost",
-      user: "root",
-      password: "Tuukka112413",
-      database: "digits"
-   });
-   console.log(newdate);
-
-   app.get('/data', (req, res) => {
-      const sqlQuery = 'SELECT * FROM days'; // Replace 'your_table_name' with the actual table name
-
-      pool.query(sqlQuery, (error, results) => {
-         if (error) {
-            console.error('Error executing query:', error);
-            return res.status(500).json({ message: 'Error fetching data from the database' });
-         }
-
-         res.json(results); // Respond with the data in JSON format
-      });
-   });
-   function saveVariable(value) {
-      console.log(value[0].targetnum1);
-      targetNum = value[0].targetnum1;
-
-   }
-
-   const port = 3000;
-   app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-   });
 }
 function changeClass(id) {
+   num1 = Number(document.getElementById('button1').innerHTML);
+   num2 = Number(document.getElementById('button2').innerHTML);
+   num3 = Number(document.getElementById('button3').innerHTML);
+   num4 = Number(document.getElementById('button4').innerHTML);
+   num5 = Number(document.getElementById('button5').innerHTML);
+   num6 = Number(document.getElementById('button6').innerHTML);
+   currentState = { "num1": [num1, num1Status], "num2": [num2, num2Status], "num3": [num3, num3Status], "num4": [num4, num4Status], "num5": [num5, num5Status], "num6": [num6, num6Status] };
+   console.log(currentState["num1"]);
+   console.log("3: " + num1);
    let madeMove = false;
    let isNumSelected = false;
    let isSymbolSelected = false;
@@ -80,7 +101,7 @@ function changeClass(id) {
       isSymbolSelected = true;
    });
 
-
+   console.log("4: " + num1);
    if ($('#' + id).hasClass('unselectedSymbol')) {
       if (isNumSelected) {
          $('#' + id).removeClass('unselectedSymbol');
@@ -103,7 +124,7 @@ function changeClass(id) {
    }
 
    if ($('#' + id).hasClass('unselectedNum')) {
-
+      console.log("5: " + num1);
       selectedNums = document.querySelectorAll('.selectedNum');
 
       if (isNumSelected) {
@@ -118,7 +139,7 @@ function changeClass(id) {
                   sym.classList.remove('selectedSymbol');
                   sym.classList.add("unselectedSymbol");
                   symbol = sym;
-
+                  console.log("6: " + num1);
                });
                console.log(symbol.id);
                if (symbol.id == "plusButton") {
@@ -135,6 +156,7 @@ function changeClass(id) {
                      $('#' + id).addClass("gettingChanged");
                   }
                   else {
+                     console.log("7: " + num1);
                      newNum = Number(document.getElementById(id).innerHTML);
 
                   }
@@ -151,20 +173,20 @@ function changeClass(id) {
                      madeMove = true;
                      number.classList.add("gettingUsed");
                      $('#' + id).addClass("gettingChanged");
-
+                     console.log("8: " + num1);
                   }
                   else {
                      newNum = Number(document.getElementById(id).innerHTML);
                   }
 
                }
-
+               madeMove = true;
                console.log(newNum);
 
 
                document.getElementById(id).innerHTML = newNum;
 
-
+               setTimeout(() => { $('#' + id).removeClass('gettingChanged'); }, 1800);
             }
 
          });
@@ -178,29 +200,110 @@ function changeClass(id) {
       $('#' + id).addClass('unselectedNum');
       selectedSyms.forEach(sym => {
          sym.classList.remove('selectedSymbol');
-         sym.classList.add("unselectedSymbol")
+         sym.classList.add("unselectedSymbol");
       });
+      console.log("10: " + num1);
+   } else if ($('#' + id).hasClass('undoButton')) {
+      currentState = states[states.length - 1];
+      console.log("hhhhh: " + currentState["num1"][0]);
+      if (currentState != undefined) {
+         if (currentState["num1"][1] == true) {
+            if ($('#button1').hasClass('gettingUsed')) {
+               $('#button1').removeClass('gettingUsed')
+               console.log("11: " + num1);
+            }
+            if ($('#button1').hasClass('gettingChanged')) {
+               $('#button1').removeClass('gettingChanged')
+            }
+            document.getElementById('button1').innerHTML = currentState["num1"][0];
+         }
+         if (currentState["num2"][1] == true) {
+            if ($('#button2').hasClass('gettingUsed')) {
+               $('#button2').removeClass('gettingUsed')
+            }
+            if ($('#button2').hasClass('gettingChanged')) {
+               $('#button2').removeClass('gettingChanged')
+            }
+            document.getElementById('button2').innerHTML = currentState["num2"][0];
+         }
+         if (currentState["num3"][1] == true) {
+            if ($('#button3').hasClass('gettingUsed')) {
+               $('#button3').removeClass('gettingUsed')
+            }
+            if ($('#button3').hasClass('gettingChanged')) {
+               $('#button3').removeClass('gettingChanged')
+            }
+            document.getElementById('button3').innerHTML = currentState["num3"][0];
+         }
+         if (currentState["num4"][1] == true) {
+            if ($('#button4').hasClass('gettingUsed')) {
+               $('#button4').removeClass('gettingUsed')
+            }
+            if ($('#button4').hasClass('gettingChanged')) {
+               $('#button4').removeClass('gettingChanged')
+            }
+            document.getElementById('button4').innerHTML = currentState["num4"][0];
+         }
+         if (currentState["num5"][1] == true) {
+            if ($('#button5').hasClass('gettingUsed')) {
+               $('#button5').removeClass('gettingUsed')
+            }
+            if ($('#button5').hasClass('gettingChanged')) {
+               $('#button5').removeClass('gettingChanged')
+            }
+            document.getElementById('button5').innerHTML = currentState["num5"][0];
+         }
+         if (currentState["num6"][1] == true) {
+            if ($('#button6').hasClass('gettingUsed')) {
+               $('#button6').removeClass('gettingUsed')
+            }
+            if ($('#button6').hasClass('gettingChanged')) {
+               $('#button6').removeClass('gettingChanged')
+            }
+            document.getElementById('button6').innerHTML = currentState["num6"][0];
+         }
+         states.splice(states.length - 1, 1);
+         console.log(states);
+      }
    }
+
+
    if (madeMove) {
-      let one = document.getElementById("button1");
-      let two = document.getElementById("button2");
-      let three = document.getElementById("button3");
-      let four = document.getElementById("button4");
-      let five = document.getElementById("button5");
-      let six = document.getElementById("button6");
-      console.log(Object.keys(moves).length);
-      moves[{ key: Object.keys(moves).length + 1, value: { one: one.innerHTML, two: two.innerHTML, three: three.innerHTML, four: four.innerHTML, five: five.innerHTML, six: six.innerHTML } }];
-      console.log(moves);
+      console.log("12: " + num1);
+      states.push(currentState);
+      console.log(states);
+      let hiddenNums = document.querySelectorAll('.gettingUsed');
+      hiddenNums.forEach(n => {
+         if (n.id == "button1") {
+            num1Status = false;
+         }
+         if (n.id == "button2") {
+            num2Status = false;
+         }
+         if (n.id == "button3") {
+            num3Status = false;
+         }
+         if (n.id == "button4") {
+            num4Status = false;
+         }
+         if (n.id == "button5") {
+            num5Status = false;
+         }
+         if (n.id == "button6") {
+            num6Status = false;
+         }
+      });
    }
 
    allNums.forEach(button => {
       if (button.innerHTML == targetNum) {
          $('#' + id).addClass('match');
 
-         setTimeout(() => { window.location.href = "game.html"; }, 1800);
+         setTimeout(() => { window.location.href = "end.html"; }, 1800);
+
       }
 
    });
 
-
+   console.log("13: " + num1);
 }
